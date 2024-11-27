f8fa2fb47e16f4d7ac40f2e420718809b3467730
inburntpancakes <gleb.children.2005@gmail.com>
2024-11-27 11:24:03 +0700

    Добавлена заглушка для доп задания

---
+++ DopZadanie/Counting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DopZadanie
+{
+    public static class Counting
+    {
+        public static int CountZeros(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
